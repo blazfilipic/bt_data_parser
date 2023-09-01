2c91a9b976645364a84016398f5e8b48473c9294
Blaž Filipič <54038162+blazfilipic@users.noreply.github.com>
2023-09-01 13:42:59 +0200

    Update bt_data_parser.c

--- bt_data_parser.c
+++ bt_data_parser.c
@@ -8,23 +8,33 @@ struct DataType {
     char* name;
 };
 
+// Define a structure for Vendor Info
+struct VendorInfo {
+    unsigned char food_temperature_high;
+    unsigned char food_temperature_low;
+    unsigned char max_possible_temperature_high;
+    unsigned char max_possible_temperature_low;
+    unsigned char battery_voltage_high;
+    unsigned char battery_voltage_low;
+};
+
 struct Data {
     unsigned char id;
     char* name;
     char* info;
+    struct VendorInfo* vendor_info; // Add a pointer to VendorInfo
 };
 
 // Define data types and their corresponding names
 struct DataType data_types[] = {
     {0x01, "Property ID"},
-    {0x06, "Random Shit"}
+    {0x06, "RandomShit"},
     {0x09, "Device Name"},
     {0xFF, "Vendor Info"},
 };
 
-
 // Function to parse and print data set
-void parse_data(const unsigned char *byte_data, size_t length, struct DataType* data_types) {
+void parse_data(const unsigned char *byte_data, size_t length, struct DataType* data_types, struct Data* data_array, size_t* data_count) {
     // Get the length from the first byte
     unsigned char data_length = byte_data[0];
 
@@ -39,7 +49,7 @@ void parse_data(const unsigned char *byte_data, size_t length, struct DataType* 
 
     // Find the corresponding data type
     char* data_type_name = "Unknown";
-    for (int i = 0; data_types[i].type != 0xFF0; i++) {
+    for (int i = 0; data_types[i].type != 0xFF; i++) {
         if (data_types[i].type == data_type) {
             data_type_name = data_types[i].name;
             break;
@@ -48,19 +58,41 @@ void parse_data(const unsigned char *byte_data, size_t length, struct DataType* 
 
     printf("Type: %hhu (%s)\n", data_type, data_type_name);
 
+    // Check if the data type is 0xFF (Vendor Info)
+    if (data_type == 0xFF) {
+        if (data_length < 8) {
+            printf("Invalid 0xFF data format.\n");
+            return;
+        }
+
+        // Create a VendorInfo structure and populate it
+        struct VendorInfo* vendor_info = (struct VendorInfo*)malloc(sizeof(struct VendorInfo));
+        vendor_info->food_temperature_high = usable_data[0];
+        vendor_info->food_temperature_low = usable_data[1];
+        vendor_info->max_possible_temperature_high = usable_data[2];
+        vendor_info->max_possible_temperature_low = usable_data[3];
+        vendor_info->battery_voltage_high = usable_data[4];
+        vendor_info->battery_voltage_low = usable_data[5];
+
+        // Update the Data structure with VendorInfo
+        data_array[*data_count].vendor_info = vendor_info;
+        (*data_count)++;
+    }
+
     // Print usable data bytes
     printf("Usable Data: ");
     for (size_t i = 0; i < data_length - 2; ++i) {
-        printf("%02x ", usable_data[i]);
+        printf("%02x", usable_data[i]);
     }
     printf("%02x\n", usable_data[data_length - 2]);
 }
 
 int main() {
-    
-    struct Data data = {0};
+    struct Data data_array[100]; // Assuming a maximum of 100 data sets
+    size_t data_count = 0;
+
     // Example hex string
-    const char *hex_string = "02010607094654303030310DFF060070CAEA80FD02AA0B1301";
+    const char *hex_string = "020106110603CA7994F75310962242118A81FA00000909697670733074725817FFF1036FF22052C0000000000A0577B8411900000000";
     size_t hex_length = strlen(hex_string);
 
     // Check if hex string length is even
@@ -81,15 +113,22 @@ int main() {
     while (offset < byte_count) {
         // Calculate length of current data set
         unsigned char data_length = byte_data[offset];
-        
+
         // Parse and process the current data set
-        parse_data(byte_data + offset, data_length, data_types);
-        
+        parse_data(byte_data + offset, data_length, data_types, data_array, &data_count);
+
         // Move the offset to the next data set
         offset += data_length;
-        
+
         // Skip the first byte of the next set
         offset += 1;
+    }
+
+    // Free allocated memory for vendor_info
+    for (size_t i = 0; i < data_count; i++) {
+        if (data_array[i].vendor_info != NULL) {
+            free(data_array[i].vendor_info);
+        }
     }
 
     // Free allocated memory
